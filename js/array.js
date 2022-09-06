6cfb9e79bb82477e249915633bc96548cb0a92df
10decoders <yasmin.iqbal@10decoders.in>
2022-09-06 15:52:02 +0530

    initial commit

---
+++ js/array.js
@@ -0,0 +1,22 @@
+//arrays and fetching elements
+
+let values = [];
+
+values = [5,6,7,8];
+
+values.push(5) // it pushes the value to  array works like append
+
+console.log(values);
+console.log(values.length);
+
+console.log(values[0])//index value starts from 0
+
+// arrays can have strings, numeric, boolean
+// can have multiple data types
+// last element cannot be used as -1 like python
+
+let val = [1,3,'yas', {fruit: 'apple'}]
+console.log(val)
+console.log(typeof(val))
+console.log(val[3])
+
---
+++ js/let_const.js
@@ -0,0 +1,6 @@
+let radius = 5;
+const pi = 3.14; //const is used for constant variables which cannot be changed 
+let area;
+
+area = pi * radius * radius
+console.log(area)
