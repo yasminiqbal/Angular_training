c6055bf19a4d28454a3f502ffdf51d8d717701c4
10decoders <yasmin.iqbal@10decoders.in>
2022-09-08 12:40:48 +0530

    Export Modules

---
+++ angular/exportNewModule/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AuthModule } from './auth/auth.module'; //import the module first
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
